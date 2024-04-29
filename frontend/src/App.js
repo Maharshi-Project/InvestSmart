42b82fcdcba31f916a48603bfedfa5b1ddcc2bfa
adityavit36 <adityavit36@gmail.com>
2024-04-29 15:23:57 +0530

    Frontend code

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NewsResearchTool from './NewsResearchTool';
+
+function App() {
+  return (
+    <div className="App">
+  <NewsResearchTool/>
+    </div>
+  );
+}
+
+export default App;
